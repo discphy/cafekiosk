0bfb9ece1b8dc11d88a5f900c410c90202f658c0
discphy <dinophy@nate.com>
2025-01-30 09:10:06 +0900

    :sparkles: BDD 스타일로 작성하기

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -82,6 +82,7 @@ class CafeKioskTest {
     @Test
     @DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
     void calculateTotalPrice() {
+        // given
         CafeKiosk cafeKiosk = new CafeKiosk();
         Americano americano = new Americano();
         Latte latte = new Latte();
@@ -89,7 +90,10 @@ class CafeKioskTest {
         cafeKiosk.add(americano);
         cafeKiosk.add(latte);
 
+        // when
         int totalPrice = cafeKiosk.calculateTotalPrice();
+
+        // then
         assertThat(totalPrice).isEqualTo(8500);
     }
 
