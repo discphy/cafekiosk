8a625e0117c74fa33261058a3a29c0c09de66c5d
discphy <dinophy@nate.com>
2025-01-31 13:02:14 +0900

    :sparkles: Business Layer 테스트

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -19,7 +19,6 @@ class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
-
     @Test
     @DisplayName("원하는 판매상태를 가진 상품들을 가져온다.")
     void findAllBySellingStatusIn() {
@@ -49,6 +48,45 @@ class ProductRepositoryTest {
         productRepository.saveAll(List.of(product1, product2, product3));
         // when
         List<Product> products = productRepository.findAllBySellingStatusIn(List.of(SELLING, HOLD));
+
+        // then
+        assertThat(products).hasSize(2)
+                .extracting("productNumber", "name", "sellingStatus")
+                .containsExactlyInAnyOrder(
+                        tuple("001", "아메리카노", SELLING),
+                        tuple("002", "카페라떼", HOLD)
+                );
+    }
+
+    @Test
+    @DisplayName("상품번호 리스트로 상품들을 가져온다.")
+    void findAllByProductNumberIn() {
+        // given
+        Product product1 = Product.builder()
+                .productNumber("001")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+        Product product2 = Product.builder()
+                .productNumber("002")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .build();
+        Product product3 = Product.builder()
+                .productNumber("003")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(STOP_SELLING)
+                .name("팥빙수")
+                .price(7000)
+                .build();
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+        // when
+        List<Product> products = productRepository.findAllByProductNumberIn(List.of("001", "002"));
 
         // then
         assertThat(products).hasSize(2)
