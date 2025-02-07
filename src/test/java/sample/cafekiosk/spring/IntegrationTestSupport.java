5def8bfde03f807f1cfe6be390ad403420d092cb
discphy <dinophy@nate.com>
2025-02-07 09:09:19 +0900

    :sparkles: 테스트 수행도 비용이다. 환경 통합하기

---
+++ src/test/java/sample/cafekiosk/spring/IntegrationTestSupport.java
@@ -0,0 +1,15 @@
+package sample.cafekiosk.spring;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import sample.cafekiosk.spring.client.mail.MailSendClient;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public abstract class IntegrationTestSupport {
+
+    // Mock 전용 Support 클래스 만드는 것을 권장한다.
+    @MockBean
+    protected MailSendClient mailSendClient;
+}
